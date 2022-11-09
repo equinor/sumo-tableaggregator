054382735d101ebfd9795a921d163718ece1c719
Daniel Berge Sollien <daniel.sollien@gmail.com>
2022-11-09 12:35:35 +0100

    Add test_tableaggregation

---
+++ tests/context.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import pytest
+from pathlib import Path
+
+# run the tests from the root dir
+TEST_DIR = Path(__file__).parent / "../"
+os.chdir(TEST_DIR)
+
+
+def add_path():
+    """Way to add package path to sys.path for testing"""
+    # Adapted from https://docs.python-guide.org/writing/structure/
+    # Turned into function because the details here didn't work
+    package_path = str(Path(__file__).parent.absolute() / "../src/")
+    while package_path in sys.path:
+        sys.path.remove(package_path)
+    sys.path.insert(0, package_path)
+
+add_path()
+
+from sumo.table_aggregation import TableAggregator
