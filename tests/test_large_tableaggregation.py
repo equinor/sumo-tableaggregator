13633b922097f2abefe2c4bed61dcafbfb443b0b
Daniel Sollien <daniel.sollien@gmail.com>
2023-09-08 13:02:11 +0200

    Change setting large test

--- tests/test_large_tableaggregation.py
+++ tests/test_large_tableaggregation.py
@@ -1,24 +1,23 @@
-"""Testing of classe TableAggregator"""
+"""Testing of class TableAggregator"""
 import logging
 from sumo.table_aggregation import TableAggregator
 from sumo.table_aggregation.utilities import timethis
 
-logging.basicConfig(level="INFO", format="%(name)s %(levelname)s: %(message)s")
+logging.basicConfig(level="DEBUG", format="%(name)s %(levelname)s: %(message)s")
 
 LOGGER = logging.getLogger()
 
 
 @timethis("Whole process")
 def test_table_aggregator():
-
     """Tests TableAggregator"""
-    test_case_name = "snorre_sumo-g2_29-2022-12-08"
-    test_table_name = "summary"
+    test_case_name = "7be66b47-4304-47d7-a949-0f7066cbcc1b"
+    test_table_name = "DROGON"
+    test_tag_name = "summary"
     aggregator = TableAggregator(
-        test_case_name, test_table_name, 0, content="timeseries", sumo_env="dev"
+        test_case_name, test_table_name, test_tag_name, "iter-0"
     )
-    aggregator.aggregate()
-    aggregator.upload()
+    aggregator.run()
 
 
 if __name__ == "__main__":
