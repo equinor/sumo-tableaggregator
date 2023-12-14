6c2f075e8ed29edc8cb56c5e5b18c118c4aa4ecb
Daniel Sollien <daniel.sollien@gmail.com>
2023-12-14 09:48:12 +0100

    Remove print statement

--- tests/test_dispatch.py
+++ tests/test_dispatch.py
@@ -95,7 +95,6 @@ def test_generate_dispatch_info(uuid, env="preview"):
         assert len(task["columns"]) <= len(task["table_index"]) + 250, "Too many columns in task"
         assert len(task["columns"]) > len(task["table_index"]), "more columns than table index"
         assert len(task["base_meta"]["data"]["spec"]["columns"]) == 0, f"data.spec.columns not nulled ({len(task['base_meta']['data']['spec']['columns'])})"
-        print(len(task["base_meta"]["data"]["spec"]["columns"]))
 
     assert len(tasks) == 104, "Should be 104 jobs for Troll case"
 
